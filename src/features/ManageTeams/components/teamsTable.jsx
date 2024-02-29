0763f9d93400e55ba29556f9e1365a1dc9a16cc1
Sarah Atef <saraatef268@gmail.com>
2024-02-29 03:32:12 +0200

    update teams table

--- src/features/ManageTeams/components/teamsTable.jsx
+++ src/features/ManageTeams/components/teamsTable.jsx
@@ -19,12 +19,47 @@ const TeamsTable = () => {
     { teamName: "back end", teamLeader: "ali", parentTeam: "db" },
   ]);
 
+<<<<<<< Updated upstream
   //   const HandelEditTeam = (team) => {
   //     setSelectedTeam(team);
   //     setPopupOpen(true);
   //     dispatch(dropDownTeamHandle(true));
   //     console.log(team, "hhh");
   //   };
+=======
+    return (
+        <div>
+            <table className="w-full text-sm text-center rtl:text-right text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Team Name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Team Leader
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Parent Team
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {teanss.map((item, index) => {
+                        return (
+                            // eslint-disable-next-line react/jsx-key
+                            <tr className=" odd:bg-gray even:bg-gray-50 ">
+                                <td className="px-6 py-4 ">{item.teamName}</td>
+                                <td className="px-6 py-4">{item.teamLeader}</td>
+                                <td className="px-6 py-4">{item.parentTeam}</td>
+                                <td className="px-6 py-4">
+                                    <Button
+                                        buttonText="Edit"
+                                        className="text-fontColor-whiteBaseColor"
+                                    />
+>>>>>>> Stashed changes
 
   return (
     <div>
