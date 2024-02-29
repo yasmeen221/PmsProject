7df3e6f6fa44fc1780bfc4f89eaa8fd8007fc9f1
yasmeen221 <yasmienmostafa38@gmail.com>
2024-02-29 15:08:17 +0200

    test

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,14 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Competencies from "../src/features/Competencies/pages/Competencies";
 import Dashboard from "../src/components/Dashboard";
 import FeedBack from "./features/FeedBack";
-import LoginForm from "./features/LogIn/components/LoginForm";
 import NotFound from "./components/NotFound";
 import Users from "./features/User&Teams/pages/Users";
 import ResetPassword from "./features/ResetPassword/pages/ResetPassword";
+import LogInPage from "./features/LogIn/pages/LogInPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LoginForm />,
+    element: <LogInPage />,
   },
   {
     path: "/resetpassword",
