4d77b605fe71f346e63696700677df3b54ef44e4
yasmeen221 <yasmienmostafa38@gmail.com>
2024-03-01 18:43:13 +0200

    test

--- src/features/ManageUsers/components/Table/UserTable.jsx
+++ src/features/ManageUsers/components/Table/UserTable.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Icons from "../../../../themes/icons";
 import Button from "../../../../components/Button/Button";
-import { deleteUser, editUsersData} from '../../slices/userSlice';
+import { deleteUser, editUsersData } from "../../slices/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { editUser } from "../../slices/editUsersSlice";
 import { handleOpenAddUserFormPopUp } from "../../slices/openAddUserFormPopUp";
@@ -12,11 +12,11 @@ export default function UserTable() {
   const handleDeleteUser = (userUserName) => {
     dispatch(deleteUser(userUserName));
   };
-  const handleEditUser = (user) => {  
+  const handleEditUser = (user) => {
     dispatch(editUser(user));
     dispatch(editUsersData(user));
     dispatch(handleOpenAddUserFormPopUp(true));
-  }
+  };
   return (
     <>
       <header className="font-bold text-lg w-[18.5rem] h-[1.668rem] my-6">
@@ -56,22 +56,32 @@ export default function UserTable() {
             </tr>
           </thead>
           <tbody>
-            
             {users.map((user, index) => (
-          <tr key={index} className={index % 2 === 0 ? 'even:bg-gray-50' : 'odd:bg-gray'}>
-          <td className="px-6 py-4">{user.firstName}</td>
-          <td className="px-6 py-4">{user.lastName}</td>
-          <td className="px-6 py-4">{user.username}</td>
-          <td className="px-6 py-4">{user.email}</td>
-          <td className="px-6 py-4">{user.position}</td>
-          <td className="px-6 py-4">{user.level}</td>
-          <td className="px-6 py-4">{user.role}</td>
-          <td className="px-6 py-4 inline-flex">
-            <Button onClick={()=>handleEditUser(user)}  iconLeft={<Icons.EditUserPage />} className="bg-transparent px-1" />
-            <Button onClick={() => handleDeleteUser(user.username)} iconLeft={<Icons.DeleteUserPage />} className="bg-transparent px-1" />
-          </td>
-        </tr>
-        ))}
+              <tr
+                key={index}
+                className={index % 2 === 0 ? "even:bg-gray-50" : "odd:bg-gray"}
+              >
+                <td className="px-6 py-4">{user.firstName}</td>
+                <td className="px-6 py-4">{user.lastName}</td>
+                <td className="px-6 py-4">{user.username}</td>
+                <td className="px-6 py-4">{user.email}</td>
+                <td className="px-6 py-4">{user.position}</td>
+                <td className="px-6 py-4">{user.level}</td>
+                <td className="px-6 py-4">{user.role}</td>
+                <td className="px-6 py-4 inline-flex">
+                  <Button
+                    onClick={() => handleEditUser(user)}
+                    iconLeft={<Icons.EditUserPage />}
+                    className="bg-transparent px-1"
+                  />
+                  <Button
+                    onClick={() => handleDeleteUser(user.username)}
+                    iconLeft={<Icons.DeleteUserPage />}
+                    className="bg-transparent px-1"
+                  />
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
