600d69c05c82431e1bddffc5773d93c42cc65dfb
sama556 <85363288+sama556@users.noreply.github.com>
2024-03-02 04:59:25 +0200

    store update

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import openPopUpSlice from "../features/FeedBack/slices/openPopUpSlice";
 import openTeamPopUpSlice from "../features/ManageTeams/slices/addTeamTogglePopUp";
 import editTeamPopUpSlice from "../features/ManageTeams/slices/editTemTogglePopUp";
-import usersReducer from '../features/ManageUsers/slices/userSlice.jsx';
-import editUsersSlice from "../features/ManageUsers/slices/editUsersSlice.jsx";
-import openAddUserFormPopUp from "../features/ManageUsers/slices/openAddUserFormPopUp.jsx";
+import usersReducer from '../features/ManageUsers/slices/userSlice';
+import editUsersSlice from "../features/ManageUsers/slices/editUsersSlice";
+import openAddUserFormPopUp from "../features/ManageUsers/slices/openAddUserFormPopUp";
 import openPopupAddLevel from "../features/ManageLevels/slices/OpenPopupLevel";
 import editLevelSlice from "../features/ManageLevels/slices/EditLevel";
-<<<<<<< HEAD
-import levelsReducer from "../features/ManageLevels/slices/LevelSlice.jsx"
+import levelsReducer from "../features/ManageLevels/slices/LevelSlice";
+import { apiSlice } from "../features/ManageTeams/slices/apis/apiSlice.js";
 
 export const store = configureStore({
-  reducer: { 
-    // Here you're providing reducers for different slices
+  reducer: {
     openPopUpSlice, // Feedback slice
     openTeamPopUpSlice, // Manage Teams slice
     editTeamPopUpSlice, // Manage Teams slice
@@ -22,22 +21,9 @@ export const store = configureStore({
     levels: levelsReducer, // Manage Levels slice
     openAddUserFormPopUp, // Manage Users slice
     editLevel: editLevelSlice, // Manage Levels slice
-    openPopupAddLevel // Manage Levels slice
+    openPopupAddLevel, // Manage Levels slice
+    [apiSlice.reducerPath]: apiSlice.reducer // API slice
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware)
 });
-=======
-import {apiSlice }from "../features/ManageTeams/slices/apis/apiSlice.js"
-export const store = configureStore({
-  reducer: {
-    openPopUpSlice,
-    openTeamPopUpSlice,
-    editTeamPopUpSlice,
-    users: usersReducer,
-    editUser: editUsersSlice,
-    openAddUserFormPopUp,
-    editLevelSlice,openPopupAddLevel,
-    [apiSlice.reducerPath]:apiSlice.reducer
-  },
-  middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware)
-})
->>>>>>> c812a6bda2e94607b13ac6229bb4f99c6979a64d
