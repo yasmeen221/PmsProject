5463895099ab8f3f3c219003dfe9fe39dd59efe8
Sarah Atef <saraatef268@gmail.com>
2024-03-02 03:07:57 +0200

    adduser updates

--- src/features/ManageLevels/components/CardsPopUp/EditLevel.jsx
+++ src/features/ManageLevels/components/CardsPopUp/EditLevel.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import Button from "../../../../components/Button/Button";
 import Header from "../../../../components/Header/Header";
