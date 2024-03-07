538ed9b447f2e5cede840e0cc4dcb6d8ccf269fd
mohamed-khaleed <mohamed.khaleed717@gmail.com>
2024-03-07 03:18:28 +0200

    make edits in normal feedback

--- src/features/FeedBack/components/CardsPopUps/GiveNormalFeedback.jsx
+++ src/features/FeedBack/components/CardsPopUps/GiveNormalFeedback.jsx
@@ -5,6 +5,9 @@ import Button from "../../../../components/Button/Button";
 import Header from "../../../../components/Header/Header";
 import Icons from "../../../../themes/icons";
 import image1 from "../../../../assets/images/girl2.png";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 const GiveNormalFeedback = () => {
   const [isPopupOpen, setPopupOpen] = useState(false);
   const [teamsBtnChecked, setTeamsBtnChecked] = useState(false);
@@ -20,6 +23,33 @@ const GiveNormalFeedback = () => {
   const handleClosePopup = () => {
     setPopupOpen(false);
   };
+
+   
+  const schema = yup
+  .object({
+    name: yup.string().required(),
+    feedback: yup.string().required(),
+    visibility: yup.string().required(),
+    team: yup.string()
+
+  })
+  .required()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const formSubmit = async (values) => {
+    console.log("hiiiiiiii")
+    console.log(values);
+  };
+
+
+
   return (
     <>
       <FormPopUp
@@ -27,56 +57,63 @@ const GiveNormalFeedback = () => {
         ClosePop={handleClosePopup}
         TitlePopUp="Give Normal  FeedBack"
         iconLeft={<Icons.ArrowLeftPop />}
-        personImage={image1}
-        personName="yasmeen"
       >
-        <div
+        <form onSubmit={handleSubmit(formSubmit)}
           className="w-[35vw] max-h-[65vh] pb-4 overflow-y-auto"
           style={{ scrollbarWidth: "none" }}
         >
           <div className="px-1 ">
             <div className="pt-4 ">
               <Header text="Name" />
-              <TextInput placeholder="Select who you will give the feedback" />
+              <TextInput  register={{ ...register("name") }} placeholder="Select who you will give the feedback" />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
             </div>
             <div className="pt-4">
               <Header text=" Feedback" />
               <div className="mt-2">
                 <textarea
+                 {...register("feedback")}
                   rows={4}
                   placeholder="Write Your honst feedback"
                   wrap="soft"
-                  id="describtion"
-                  name="describtion"
-                  onChange={(e) => console.log(e.target.value)}
+                  id="feedback"
+                  name="feedback"
                   className="min-h-20 resize-none block max-h-20 bg-white w-full text-body1Size rounded-buttonRadius border-0  py-2.5 px-2  shadow-sm ring-1 ring-fontColor-outLineInputColor  placeholder:text-fontColor-placeHolderColor focus:ring-2   focus:ring-buttonColor-baseColor focus:outline-none sm:text-sm sm:leading-6"
                 />
               </div>
+              {errors.feedback && (
+                <p className="text-red-500">{errors.feedback.message}</p>
+              )}
             </div>
             <div className="pt-4 mb-4">
               <Header text="Visibility" />
               <div className="flex flex-wrap ">
                 <label className="inline-flex items-center mr-4 mb-2">
-                  <input type="radio" className="w-4 h-4" name="radioGroup" />
+                  <input type="radio" value="mangerOnly"  {...register('visibility')} className="w-4 h-4" name="visibility" />
                   <span className="ml-2 font-custom text-buttonFontSize font-buttonWeight text-fontColor-blackBaseColor">
                     Manger only
                   </span>
                 </label>
 
                 <label className="inline-flex items-center mr-4 mb-2">
-                  <input type="radio" className="w-4 h-4" name="radioGroup" />
+                  <input type="radio"  value="employeeOnly" {...register('visibility')} className="w-4 h-4" name="visibility" />
                   <span className="ml-2 font-custom text-buttonFontSize font-buttonWeight text-fontColor-blackBaseColor">
                     Employee only
                   </span>
                 </label>
 
                 <label className="inline-flex items-center mr-4 mb-2">
-                  <input type="radio" className="w-4 h-4" name="radioGroup" />
+                  <input type="radio"  value="mangerAndEmployee"  {...register('visibility')} className="w-4 h-4" name="visibility" />
                   <span className="ml-2 font-custom text-buttonFontSize font-buttonWeight text-fontColor-blackBaseColor">
                     Manger and Employee
                   </span>
                 </label>
               </div>
+              {errors.visibility&& (
+                <p className="text-red-500">{errors.visibility.message}</p>
+              )}
             </div>
             {/* switch */}
             <div className="inline-flex justify-between items-center w-full pb-4">
@@ -90,7 +127,6 @@ const GiveNormalFeedback = () => {
                 <label className="inline-flex items-center cursor-pointer">
                   <input
                     type="checkbox"
-                    value=""
                     className="sr-only peer"
                     onChange={(e) => {
                       setTeamsBtnChecked(e.target.checked);
@@ -104,7 +140,8 @@ const GiveNormalFeedback = () => {
             {teamsBtnChecked && (
               <div className="relative my-2 ">
                 <select
-                  onChange={(e) => setTeam((team) => e.target.value)}
+                   {...register("team")}
+                   name="team"
                   className={`block appearance-none w-full bg-white border-0 py-2.5 px-2 ring-1 ring-inset ring-fontColor-outLineInputColor  rounded-buttonRadius shadow-sm   focus:shadow-outline focus:ring-2 focus:ring-buttonColor-baseColor focus:outline-none ${team == "" ? "text-fontColor-placeHolderColor" : "text-fontColor-blackBaseColor"}`}
                 >
                   <option value="">select team</option>
@@ -115,17 +152,21 @@ const GiveNormalFeedback = () => {
                 <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   <Icons.ArrowDownBlack />
                 </div>
+                {errors.team&& (
+                <p className="text-red-500">{errors.team.message}</p>
+              )}
               </div>
             )}
           </div>
-        </div>
-        <div className="flex items-center justify-end border-t border-gray-200 py-3 mx-1 ">
+          <div className="flex items-center justify-end border-t border-gray-200 py-3 mx-1 ">
           <Button
             className="px-10 py-2.5 text-fontColor-whiteBaseColor"
             buttonText="Give Feedback"
-            onClick={handleClosePopup}
+            // onClick={handleClosePopup}
           />
         </div>
+        </form>
+     
       </FormPopUp>
       {/* <Button
         buttonText="Formal feedback"
