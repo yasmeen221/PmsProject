4451eec0a35edee1691e1b656a8599957c48d1db
mohamed-khaleed <mohamed.khaleed717@gmail.com>
2024-03-07 17:00:10 +0200

    Revert "Merge pull request #24 from yasmeen221/add-comptency-feature"
    This reverts commit 22bcf4acf8a7a9e2c59e6b39cd7a7d57e2a7ed86, reversing
    changes made to 192def748996661152ccfd81f83739af2ea8a5c9.

--- src/features/Competencies/components/CardsPopUps/AddCompetency.jsx
+++ src/features/Competencies/components/CardsPopUps/AddCompetency.jsx
@@ -6,93 +6,10 @@ import Header from "../../../../components/Header/Header";
 import TextInput from "../../../../components/TextInput/TextInput";
 function AddCompetency({ open }) {
   const [isPopupOpen, setPopupOpen] = useState(false);
-
-  const [teamsBtnChecked, setTeamsBtnChecked] = useState(false);
-  const [teamsAssigned, setTeamsAssigned] = useState([]); 
-  const [formlevels,setFormLevels] = useState([]); 
-  const [descriptions, setDescriptions] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [levelErrorMsg, setLevelErrorMsg] = useState(false);
-  const [teamsErrorMsg, setTeamsErrorMsg] = useState(false);
-  const seniorityLevels = formlevels?.map((level, index) => ({
-    level,
-    description: descriptions[index],
-  }));
-
- 
-  const schema = yup
-    .object({
-      name: yup.string().required("Name is required"),
-      category: yup.string().required("Category is required"),
-      defaultDescription: yup.string()
-    })
-    .required();
-
-    const {
-      data: teamsNames,
-    } = useGetTeamsNameQuery();
-    const teamsArray = teamsNames?.data?.teamsNames ;
-    const teamsOptions = teamsArray?.map(team => ({ value: team._id, label: team.teamName }));
-
-    const {
-      data: levels,
-
-    } = useGetLevelQuery();
-
-    const levelsArray = levels?.data?.levels ;
-    const levelsOptions = levelsArray?.map(level => ({ value: level._id, label: level.levelName }));
-
-    const handleTeamChange = (selectedOptions) => {
-      const teams = selectedOptions.map((team) => setTeamsAssigned([...teamsAssigned, team?.value])); 
-    };
-
-
-    const handleLevelChange = (selectedOptions) => {
-      const levels = selectedOptions.map((level) => setFormLevels([...formlevels, level.value]));
-    };
-    
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const formSubmit = async (values) => {
-    try {
-
-      console.log(formlevels.length)
-
-      if (formlevels.length === 0) {
-        setLevelErrorMsg(true)
-        console.log('Please add levels first')
-      
-      } 
-
-      if (teamsAssigned.length === 0 && teamsBtnChecked) {
-        setTeamsErrorMsg(true)
-        console.log('Please add teams first')
-        
-      }
-      
-       if (teamsAssigned.length === 0 && seniorityLevels.length === 0 && formlevels.length===0) return
-      const dataToSend = {
-        ...values,
-        seniorityLevels,
-        teamsAssigned,
-      };
-      console.log('Data to send:', dataToSend)
-      const response = await axiosInstance.post('/competency', dataToSend);
-      console.log('Backend response:', response.data);
-      setPopupOpen(false);
-    } catch (error) {
-      console.error('Error sending data to the backend:', error);
-    }
-  };
-
-
+  const [catogery, setCatogery] = useState("");
+  const [addToogle, setAddToggle] = useState(false);
+  const [team, setTeam] = useState("");
+  const [level, setLevel] = useState("");
   const handleOpenPopup = () => {
     setPopupOpen(true);
   };
@@ -182,18 +99,20 @@ function AddCompetency({ open }) {
               </label>
             </div>
           </div>
-
-          {teamsBtnChecked && (
-     
-            <div>
-               <Select
-                options={teamsOptions}
-                onChange={handleTeamChange}
-                isMulti
-                  closeMenuOnSelect={false} 
-                   />
-                   {teamsErrorMsg  && <p className="text-red-500">Please add teams first</p>}
-
+          {addToogle && (
+            <div className="relative my-2 transition-all duration-1000 ">
+              <select
+                onChange={(e) => setTeam((team) => e.target.value)}
+                className={`block appearance-none w-full bg-white border-0    py-2.5 px-2 ring-1 ring-inset ring-fontColor-outLineInputColor  rounded-buttonRadius shadow-sm   focus:shadow-outline focus:ring-2 focus:ring-buttonColor-baseColor focus:outline-none ${team == "" ? "text-fontColor-placeHolderColor" : "text-fontColor-blackBaseColor"} `}
+              >
+                <option value="">select team</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <Icons.ArrowDownBlack />
+              </div>
             </div>
           )}
           <div className="my-2">
@@ -202,42 +121,18 @@ function AddCompetency({ open }) {
               Select Levels to customize competency descriptions for each career
               path level.
             </p>
-
-            <div className="relative mt-2">   
-               <Select
-                options={levelsOptions}
-                onChange={handleLevelChange}
-                isMulti
-                  closeMenuOnSelect={false} // Keep the dropdown open after selection
-                   />
-                  
-                   {levelErrorMsg  &&  <p className="text-red-500">Please add levels first</p>}
-            </div>
-          </div>
-
-          {formlevels.length!==0  && levelsArray?.map((level, index) => {
-            return(
-              
-            <div key={index} className= "relative my-2 transition-all duration-1000 " >
-              <div className="my-2">
-                <div className="flex items-center justify-between">
-                  <Header text={level?.levelName} htmlFor="levelDescription" />
-                  <div className=" cursor-pointer flex items-center justify-center rounded-sm  text-red-500 w-4 h-4  border border-red-500" >
-                    -
-                  </div>
-                </div>
-
-                <div className="mt-2">
-                  <textarea
-                    rows={4}
-                    placeholder={`description for ${level?.levelName}`}
-                    wrap="soft"
-                    className="min-h-20 resize-none block max-h-20 bg-white w-full text-body1Size rounded-buttonRadius border-0  py-2.5 px-2  shadow-sm ring-1 ring-fontColor-outLineInputColor  placeholder:text-fontColor-placeHolderColor focus:ring-2   focus:ring-buttonColor-baseColor focus:outline-none sm:text-sm sm:leading-6"
-                    onChange={(e) => handleDescriptionChange(index, e.target.value)}
-                  />
-                 
-                </div>
-
+            <div className="relative mt-2">
+              <select
+                onChange={(e) => setLevel((level) => e.target.value)}
+                className={`block appearance-none w-full bg-white border-0    py-2.5 px-2 ring-1 ring-inset ring-fontColor-outLineInputColor  rounded-buttonRadius shadow-sm   focus:shadow-outline focus:ring-2 focus:ring-buttonColor-baseColor focus:outline-none ${level == "" ? "text-fontColor-placeHolderColor" : "text-fontColor-blackBaseColor"} `}
+              >
+                <option value="">select category</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <Icons.ArrowDownBlack />
               </div>
             </div>
           </div>
