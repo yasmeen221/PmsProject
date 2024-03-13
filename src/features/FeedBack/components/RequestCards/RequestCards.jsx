79e35cd5ab63a27b37b020301b72b63f0259c2e5
yasmeen221 <yasmienmostafa38@gmail.com>
2024-03-13 10:02:48 +0200

    test

--- src/features/FeedBack/components/RequestCards/RequestCards.jsx
+++ src/features/FeedBack/components/RequestCards/RequestCards.jsx
@@ -7,19 +7,37 @@ import Icons from "../../../../themes/icons";
 import Pagination from "../../../../components/Pagination/Pagination";
 import { getDataWithPagination } from "../../utils/helperFunctions";
 function RequestCards() {
-  const [numberOfPages, setNumberOfPages] = useState(0)
-  const [data, setData] = useState([])
-  const [isLoadingFeedback, setIsLoadingFeedback] = useState(false)
-  const userData = useSelector((state) => state.persistantReducer.userDataReducer.userData);
-  const [currentPage, setCurrentPage] = useState(1)
+  const [numberOfPages, setNumberOfPages] = useState(0);
+  const [data, setData] = useState([]);
+  const [isLoadingFeedback, setIsLoadingFeedback] = useState(false);
+  const userData = useSelector(
+    (state) => state.persistantReducer.userDataReducer.userData,
+  );
+  const [currentPage, setCurrentPage] = useState(1);
   const userId = userData.length > 0 ? jwtDecode(userData).userId : "";
   useEffect(() => {
-    getDataWithPagination(setIsLoadingFeedback, setData, 1, userId, setNumberOfPages,true,false,false)
-
-  }, [])
+    getDataWithPagination(
+      setIsLoadingFeedback,
+      setData,
+      1,
+      userId,
+      setNumberOfPages,
+      true,
+      false,
+      false,
+    );
+  }, []);
   const handlePageClick = (event) => {
-    getDataWithPagination(setIsLoadingFeedback, setData, event.selected + 1, userId, setNumberOfPages,true,false,false)
-
+    getDataWithPagination(
+      setIsLoadingFeedback,
+      setData,
+      event.selected + 1,
+      userId,
+      setNumberOfPages,
+      true,
+      false,
+      false,
+    );
   };
   return (
     <>
@@ -34,31 +52,37 @@ function RequestCards() {
             <Icons.Loading />
           </div>
         ) : data.length > 0 ? (
-          data.filter(item=>item.feedbackMainData.userIdTo).map((item, index) => (
-            <RequestCardItem
-              key={item.feedbackMainData._id}
-              text={item.feedbackMainData.message}
-              image={image2}
-              name={`${ item.feedbackMainData.userIdTo.firstName} ${ item.feedbackMainData.userIdTo.lastName }`}
-              date={item.feedbackMainData.createdAt.substring(0, 10)}
-              cardId={item.feedbackMainData._id}
-              getDataWithPagination={getDataWithPagination}
-              currentPage={currentPage}
-              setIsLoadingFeedback={setIsLoadingFeedback}
-              setData={setData}
-              userId={userId}
-              setNumberOfPages={setNumberOfPages}
-            />
-          ))
-        ) : isLoadingFeedback == false &&
-          data.length == 0 ? (
+          data
+            .filter((item) => item.feedbackMainData.userIdTo)
+            .map((item, index) => (
+              <RequestCardItem
+                key={item.feedbackMainData._id}
+                text={item.feedbackMainData.message}
+                image={image2}
+                name={`${item.feedbackMainData.userIdTo.firstName} ${item.feedbackMainData.userIdTo.lastName}`}
+                date={item.feedbackMainData.createdAt.substring(0, 10)}
+                cardId={item.feedbackMainData._id}
+                getDataWithPagination={getDataWithPagination}
+                currentPage={currentPage}
+                setIsLoadingFeedback={setIsLoadingFeedback}
+                setData={setData}
+                userId={userId}
+                setNumberOfPages={setNumberOfPages}
+              />
+            ))
+        ) : isLoadingFeedback == false && data.length == 0 ? (
           <div className="w-full flex flex-row justify-center">
             <p>There is No Requestes Exist</p>
           </div>
         ) : (
           ""
         )}
-        {<Pagination handlePageClick={handlePageClick} numberOfPages={numberOfPages} />}
+        {
+          <Pagination
+            handlePageClick={handlePageClick}
+            numberOfPages={numberOfPages}
+          />
+        }
       </main>
     </>
   );
