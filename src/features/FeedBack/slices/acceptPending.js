efdcf7c9633b9e3a8f1743bbf19b3e74744975f7
yasmeen221 <yasmienmostafa38@gmail.com>
2024-03-19 00:45:29 +0200

    test

--- src/features/FeedBack/slices/acceptPending.js
+++ src/features/FeedBack/slices/acceptPending.js
@@ -4,36 +4,36 @@ const initialState = {
   username: "",
   id: "",
   cardId: "",
-  feedbackCompetencies:"",
-  acceptDone: false
+  feedbackCompetencies: "",
+  acceptDone: false,
 };
 const confirmSlice = createSlice({
   name: "confirmSlice",
   initialState: initialState,
   reducers: {
-
     setUserName: (state, action) => {
-      state.username = action.payload
+      state.username = action.payload;
     },
     setFromId: (state, action) => {
-      state.id = action.payload
+      state.id = action.payload;
     },
     setCardId: (state, action) => {
-      state.cardId = action.payload
-    }, setAcceptPending: (state, action) => {
-      state.acceptDone = action.payload
-    },setFeedbackCompetencies:(state,action)=>{
-      state.feedbackCompetencies=action.payload
-    }
+      state.cardId = action.payload;
+    },
+    setAcceptPending: (state, action) => {
+      state.acceptDone = action.payload;
+    },
+    setFeedbackCompetencies: (state, action) => {
+      state.feedbackCompetencies = action.payload;
+    },
   },
 });
 export const {
-
   setUserName,
   setFromId,
   setCardId,
   setAcceptPending,
-  setFeedbackCompetencies
+  setFeedbackCompetencies,
 } = confirmSlice.actions;
 
 export default confirmSlice.reducer;
