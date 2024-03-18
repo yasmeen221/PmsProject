e85458a109bb026e49b0245559236c724d825df1
yasmeen221 <yasmienmostafa38@gmail.com>
2024-03-19 01:25:00 +0200

    handel logic bug in feedbackforsomeone and pendingcards

--- src/features/FeedBack/components/PendingCards/PendingCardItem.jsx
+++ src/features/FeedBack/components/PendingCards/PendingCardItem.jsx
@@ -5,25 +5,59 @@ import ImageStyle from "../../../../components/ImageStyle/ImageStyle";
 import { useDispatch, useSelector } from "react-redux";
 import { HandelOpenPopUpDelete } from "../../../ManageTeams/slices/HandelOpenDelete";
 import ConfirmDelete from "../../../../components/Delete/ConfirmDelete";
-import  {  setIdToDeletePendingPage, toggleNormalFeedback } from "../../slices/openPopUpSlice";
+import {
+  setIdToDeletePendingPage,
+  toggleNormalFeedback,
+} from "../../slices/openPopUpSlice";
 import GiveNormalFeedback from "../CardsPopUps/GiveNormalFeedback";
 import { deleteFeedback } from "../../slices/Api/feedbackApi";
-import { setCardId, setFeedbackCompetencies, setFromId, setUserName } from "../../slices/acceptPending";
+import {
+  setCardId,
+  setFeedbackCompetencies,
+  setFromId,
+  setUserName,
+} from "../../slices/acceptPending";
 
-export default function PendingCardItem({ image, name, date, text, cardId ,feedBackMetaData,fromName,fromId,getDataWithPagination,currentPage,setIsLoadingFeedback,setData,userId,setNumberOfPages}) {
+export default function PendingCardItem({
+  image,
+  name,
+  date,
+  text,
+  cardId,
+  feedBackMetaData,
+  fromName,
+  fromId,
+  getDataWithPagination,
+  currentPage,
+  setIsLoadingFeedback,
+  setData,
+  userId,
+  setNumberOfPages,
+}) {
   const dispatch = useDispatch();
   const oPenPopUp = useSelector(
     (state) => state.openPopUpConfirmDeleteSlice.open,
   );
-  const id = useSelector(state => state.openPopUpSlice.idToDeletePendingPage)
-  const openNormalFeedbackPopUp = useSelector(state => state.openPopUpSlice.normalFeedbackPopup)
-  const handleDelete=(cardId)=>{
-    deleteFeedback(cardId).then((res)=>{
-      getDataWithPagination(setIsLoadingFeedback,setData,currentPage,userId,setNumberOfPages,false,false,true)
-      console.log(res)
-    })
-  }
-  useEffect(() => { }, [id])
+  const id = useSelector((state) => state.openPopUpSlice.idToDeletePendingPage);
+  const openNormalFeedbackPopUp = useSelector(
+    (state) => state.openPopUpSlice.normalFeedbackPopup,
+  );
+  const handleDelete = (cardId) => {
+    deleteFeedback(cardId).then((res) => {
+      getDataWithPagination(
+        setIsLoadingFeedback,
+        setData,
+        currentPage,
+        userId,
+        setNumberOfPages,
+        false,
+        false,
+        true,
+      );
+      console.log(res);
+    });
+  };
+  useEffect(() => {}, [id]);
 
   return (
     <div className=" flex flex-col justify-between w-[32%] h-[11.53rem]  border px-8 py-6  rounded-2xl  border-borderColor-100">
@@ -33,9 +67,7 @@ export default function PendingCardItem({ image, name, date, text, cardId ,feedB
             <ImageStyle src={image} />
           </div>
           <div className=" w-[75%]">
-            <p className="font-md font-medium  text-sm w-[8.654rem]">
-              {name}
-            </p>
+            <p className="font-md font-medium  text-sm w-[8.654rem]">{name}</p>
             <p className="font-md font-medium text-sm text-deleteColor-400">
               {date}
             </p>
@@ -47,22 +79,48 @@ export default function PendingCardItem({ image, name, date, text, cardId ,feedB
             icon={<Icons.GreenFeedback />}
             bgColor="#EBF5EF"
             hoverColor=" #329B5C"
-            confirmButtonClick={() => { dispatch(toggleNormalFeedback(true)), dispatch(setUserName(fromName)),dispatch(setCardId(cardId)),dispatch(setFromId(fromId)),dispatch(setFeedbackCompetencies(feedBackMetaData)) }}
+            confirmButtonClick={() => {
+              dispatch(toggleNormalFeedback(true)),
+                dispatch(setUserName(fromName)),
+                dispatch(setCardId(cardId)),
+                dispatch(setFromId(fromId)),
+                dispatch(setFeedbackCompetencies(feedBackMetaData)),
+                console.log("metaData", feedBackMetaData),
+                console.log(
+                  "datatat",
+                  feedBackMetaData.find(
+                    (item, index) => item.name == "feedbackAbout",
+                  )?.value
+                    ? feedBackMetaData.find(
+                        (item, index) => item.name == "feedbackAbout",
+                      ).value
+                    : "",
+                );
+            }}
           />
 
           <PenndingButton
             icon={<Icons.RedFeedback />}
             bgColor="#FBE8E8"
             hoverColor=" #DB1A1A"
-            deleteButtonClick={() => {dispatch(HandelOpenPopUpDelete(true)),dispatch(setIdToDeletePendingPage(cardId))}}
+            deleteButtonClick={() => {
+              dispatch(HandelOpenPopUpDelete(true)),
+                dispatch(setIdToDeletePendingPage(cardId));
+            }}
           />
         </div>
       </div>
-      <div className="text-xs text-slate-700">
-        {text}
-      </div>
-      {oPenPopUp && <ConfirmDelete deleteText="Are you sure to Decline? " confirmButtonText="Decline" onConfirm={() => {handleDelete(id)}} />}
-      {openNormalFeedbackPopUp && <GiveNormalFeedback />} 
+      <div className="text-xs text-slate-700">{text}</div>
+      {oPenPopUp && (
+        <ConfirmDelete
+          deleteText="Are you sure to Decline? "
+          confirmButtonText="Decline"
+          onConfirm={() => {
+            handleDelete(id);
+          }}
+        />
+      )}
+      {openNormalFeedbackPopUp && <GiveNormalFeedback />}
     </div> //card
   );
 }
--- src/features/FeedBack/components/PendingCards/PendingCards.jsx
+++ src/features/FeedBack/components/PendingCards/PendingCards.jsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import PendingCardItem from "./PendingCardItem";
 import image1 from "../../../../assets/images/boy4.png";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,22 +9,41 @@ import { getDataWithPagination } from "../../utils/helperFunctions";
 import { setAcceptPending } from "../../slices/acceptPending";
 
 export default function PendingCards() {
-  const dispatch=useDispatch()
-  const [numberOfPages, setNumberOfPages] = useState(0)
+  const dispatch = useDispatch();
+  const [numberOfPages, setNumberOfPages] = useState(0);
 
-  const [isLoadingFeedback, setIsLoadingFeedback] = useState(false)
-  const [data, setData] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
+  const [isLoadingFeedback, setIsLoadingFeedback] = useState(false);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const acceptDone = useSelector((state) => state.confirmSlice.acceptDone);
-  const userData = useSelector(state => state.persistantReducer.userDataReducer.userData)
-  const userId = userData.length > 0 ? jwtDecode(userData).userId : ""
+  const userData = useSelector(
+    (state) => state.persistantReducer.userDataReducer.userData,
+  );
+  const userId = userData.length > 0 ? jwtDecode(userData).userId : "";
   useEffect(() => {
-    getDataWithPagination(setIsLoadingFeedback, setData, 1, userId, setNumberOfPages,false,false,true)
-    dispatch(setAcceptPending(false))
-  }, [acceptDone]) //to refresh when accept pending 
+    getDataWithPagination(
+      setIsLoadingFeedback,
+      setData,
+      1,
+      userId,
+      setNumberOfPages,
+      false,
+      false,
+      true,
+    );
+    dispatch(setAcceptPending(false));
+  }, [acceptDone]); //to refresh when accept pending
   const handlePageClick = (event) => {
-    getDataWithPagination(setIsLoadingFeedback, setData, event.selected + 1, userId, setNumberOfPages,false,false,true)
-
+    getDataWithPagination(
+      setIsLoadingFeedback,
+      setData,
+      event.selected + 1,
+      userId,
+      setNumberOfPages,
+      false,
+      false,
+      true,
+    );
   };
   return (
     <>
@@ -37,34 +56,52 @@ export default function PendingCards() {
           <div className="w-full flex flex-row justify-center">
             <Icons.Loading />
           </div>
-        ) : data.filter((item) => item.feedbackMainData.userIdFrom).length > 0 ? ( //filter to make sure the sender account dosent deleted
-          data.filter((item) => item.feedbackMainData.userIdFrom).map((item, index) => (
-            <PendingCardItem
-              key={item.feedbackMainData._id}
-              cardId={item.feedbackMainData._id} //the id of the card to accept or delete it
-              text={item.feedbackMainData.message}
-              name={`${item.feedbackMainData.userIdFrom.firstName} ${item.feedbackMainData.userIdFrom.lastName}`}
-              date={item.feedbackMainData.createdAt.substring(0, 10)}
-              image={image1}
-              feedBackMetaData={item.feedBackMetaData}   //to display competencies in the feedback popup when press accept           
-              fromName={item.feedbackMainData.userIdFrom.username} //to put it in dropdown label when make normal feedback when press ✅ ==>(the name of the person that want feedback(it will be the name of the reciever when accept the request and send normal feedback))
-              fromId={item.feedbackMainData.userIdFrom._id} //to put in normal feedback submit ==>(the id of the person that want feedback(it will be the id of the reciever when accept the request and send normal feedback))
-              getDataWithPagination={getDataWithPagination} //to refresh when delete  item when press delete in pendingcarditem page
-              currentPage={currentPage} //to know your current page in pagination to refresh it
-              setIsLoadingFeedback={setIsLoadingFeedback} //to make set to loader in pendingcards by the pendingcarditem when delete item
-              setData={setData} //to set the array of feedbacks displayed in the page
-              userId={userId} //to get userIdTo=userId
-              setNumberOfPages={setNumberOfPages} //to set number of pages 
-            />
-          ))
-        ) : isLoadingFeedback == false && data.filter((item) => item.feedbackMainData.userIdFrom).length == 0 ? (
+        ) : data.filter((item) => item.feedbackMainData.userIdFrom).length >
+          0 ? ( //filter to make sure the sender account dosent deleted
+          data
+            .filter((item) => item.feedbackMainData.userIdFrom)
+            .map((item, index) => (
+              <PendingCardItem
+                key={item.feedbackMainData._id}
+                cardId={item.feedbackMainData._id} //the id of the card to accept or delete it
+                text={item.feedbackMainData.message}
+                name={`${item.feedbackMainData.userIdFrom.firstName} ${item.feedbackMainData.userIdFrom.lastName}`}
+                date={item.feedbackMainData.createdAt.substring(0, 10)}
+                image={image1}
+                feedBackMetaData={item.feedBackMetaData} //to display competencies in the feedback popup when press accept
+                fromName={item.feedbackMainData.userIdFrom.username} //to put it in dropdown label when make normal feedback when press ✅ ==>(the name of the person that want feedback(it will be the name of the reciever when accept the request and send normal feedback))
+                fromId={
+                  item.feedBackMetaData.find(
+                    (item, index) => item.name == "feedbackAbout",
+                  )?.value
+                    ? item.feedBackMetaData.find(
+                        (item, index) => item.name == "feedbackAbout",
+                      ).value
+                    : item.feedbackMainData.userIdFrom._id
+                } //to put in normal feedback submit ==>(the id of the person that want feedback(it will be the id of the reciever when accept the request and send normal feedback))
+                getDataWithPagination={getDataWithPagination} //to refresh when delete  item when press delete in pendingcarditem page
+                currentPage={currentPage} //to know your current page in pagination to refresh it
+                setIsLoadingFeedback={setIsLoadingFeedback} //to make set to loader in pendingcards by the pendingcarditem when delete item
+                setData={setData} //to set the array of feedbacks displayed in the page
+                userId={userId} //to get userIdTo=userId
+                setNumberOfPages={setNumberOfPages} //to set number of pages
+              />
+            ))
+        ) : isLoadingFeedback == false &&
+          data.filter((item) => item.feedbackMainData.userIdFrom).length ==
+            0 ? (
           <div className="w-full flex flex-row justify-center">
             <p>There is No Pending Requests Exist</p>
           </div>
-        ) :""
+        ) : (
+          ""
+        )}
+        {
+          <Pagination
+            handlePageClick={handlePageClick}
+            numberOfPages={numberOfPages}
+          />
         }
-        {<Pagination handlePageClick={handlePageClick} numberOfPages={numberOfPages} />}
-
       </main>
     </>
   );
